4aea50c07ad2e97ce46e7eefb56308828cda76c6
cesarvega <cesarvega.col@gmail.com>
2021-07-08 14:59:10 -0400

    app fully working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,6 @@ import { SchedulerComponent } from './scheduler/scheduler.component';
 import { BsrComponent } from './bsr/bsr.component';
 import { NW3Component } from './nw3/nw3.component';
 import { BmxComponent } from './bmx/bmx.component';
-import { BmxCreatorComponent } from './bmx-creator/bmx-creator.component';
 
 const routes: Routes = [
   // {// NAME VOTE LOGIN https://bipresents.com/namevote/login?project=Test_WELL_PLATFORM
@@ -26,25 +25,25 @@ const routes: Routes = [
   //   path: ':id',
   //   component: SchedulerComponent  
   // },
-  // {//BSR  color of the bar #002f5b
+  // {//BSR
   //   path: ':id',
   //   component: BsrComponent
   // },
-  {//BI BRAND MATRIX CREATOR
-    path: ':id',
-    component: BmxCreatorComponent
-  },
+  // {//BI BRAND MATRIX
+  //   path: ':id',
+  //   component: BsrComponent
+  // },
   // {//BI BRAND MATRIX
   //   path: ':id',
   //   component: BmxComponent
   // },
-  // {//NW 3 BI PRESENTS NOMENCLATURE WORKSHOP V.3.0
-  //   path: ':id',
-  //   component: NW3Component
-  // },
+  {//NW 3 BI PRESENTS NOMENCLATURE WORKSHOP V.3.0
+    path: ':id',
+    component: NW3Component
+  },
   {
     path: '',
-    redirectTo: '/',
+    redirectTo: '/food',
     pathMatch: 'full'
   } 
 ];
