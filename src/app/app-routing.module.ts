84eab1c2b119b18514d40680dd516364e8cfca50
cesarvega <cesarvega.col@gmail.com>
2021-07-08 15:16:58 -0400

    app fully working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { SchedulerComponent } from './scheduler/scheduler.component';
 import { BsrComponent } from './bsr/bsr.component';
 import { NW3Component } from './nw3/nw3.component';
 import { BmxComponent } from './bmx/bmx.component';
+import { BmxCreatorComponent } from './bmx-creator/bmx-creator.component';
 
 const routes: Routes = [
   // {// NAME VOTE LOGIN https://bipresents.com/namevote/login?project=Test_WELL_PLATFORM
@@ -29,21 +30,21 @@ const routes: Routes = [
   //   path: ':id',
   //   component: BsrComponent
   // },
-  // {//BI BRAND MATRIX
-  //   path: ':id',
-  //   component: BsrComponent
-  // },
-  // {//BI BRAND MATRIX
+  {//BI BRAND MATRIX CREATOR
+    path: ':id',
+    component: BmxCreatorComponent
+  },
+  // {//BI BRAND MATRIX MOBILE
   //   path: ':id',
   //   component: BmxComponent
   // },
-  {//NW 3 BI PRESENTS NOMENCLATURE WORKSHOP V.3.0
-    path: ':id',
-    component: NW3Component
-  },
+  // {//NW 3 BI PRESENTS NOMENCLATURE WORKSHOP V.3.0
+  //   path: ':id',
+  //   component: NW3Component
+  // },
   {
     path: '',
-    redirectTo: '/food',
+    redirectTo: '/TEST_BI_Katakana_Phonetics',
     pathMatch: 'full'
   } 
 ];
