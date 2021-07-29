079654b957dd418072fa578baef3082074b7298f
cesarvega <cesarvega.col@gmail.com>
2021-07-29 19:05:48 -0400

    respondants table layout updated

--- src/app/bmx/bmx-creator/survey-creation-design/survey-creation-design.component.ts
+++ src/app/bmx/bmx-creator/survey-creation-design/survey-creation-design.component.ts
@@ -7,7 +7,7 @@ import { Component, ElementRef, EventEmitter, Inject, Input, OnInit, Output, Vie
 })
 export class SurveyCreationDesignComponent implements OnInit {
 
-  @Input() isMenuActive11 ;
+  @Input() isMenuActive11;
   model = {
     editorData: '',
     namesData: ''
@@ -16,8 +16,10 @@ export class SurveyCreationDesignComponent implements OnInit {
   ckconfig: any;
   selectedIndex: any
   sampleHtml = `<p style="text-align:center;color:red">Instructions</p>\n\n<p style=\"text-align:justify\"><br />\nPlease select at least three &quot;themes&quot; you would consider to move forward for the Line Draw Family.</p>\n\n<p style=\"text-align:justify\"><strong>What do we mean by &quot;theme&quot;:</strong></p>\n\n<p style=\"text-align:justify\">We will develop names that pertain to an overarching theme. Each individual name candidate will have potential to be used as an ingredient brand to be used across all Line Draw Family concepts or as it pertains to each individual concept. In the latter scenario, we will develop names with a common word part and this word part will be included in each concept name. For example, if you choose the &quot;Optimized&quot; theme, we will develop candidates around the Op/Opt/Opti word parts.</p>\n\n<p style=\"text-align:justify\"><strong>How many themes should I vote on?</strong></p>\n\n<p style=\"text-align:justify\">You can select as many as you&rsquo;d like but we request that you select at least 3 themes. Based on the vote, we will select three to five themes for full creative exploration. How do I provide a vote? To make a selection, simply click the checkbox to the left of the desired name candidate. After you make a selection, you will be asked to rate that theme based on your own personal preference on a scale from 1 to 7, 1 being neutral and 7 being the most liked.</p>\n\n<p style=\"text-align:justify\">Once you have finished your selections, please click the &quot;Continue&quot; button on the bottom of the page to proceed to the next evaluation section.</p>\n`
+  sampleHtml2 = `<p style="text-align:center;color:red">MORE TEXT OR PARGRAPTH</p>`
   selectedOption: any;
 
+  displayInstructions = false;
   // SURVEY CREATOR VARIABLES & SCHEME
   brandMatrixObjects = [
     {
@@ -26,7 +28,7 @@ export class SurveyCreationDesignComponent implements OnInit {
       componentSettings: [{ fontSize: '16px', fontFace: 'Arial', fontColor: 'red' }],
     },
     {
-      componentType: 'text-editor',
+      componentType: 'instructions',
       componentText: this.sampleHtml,
       componentSettings: [{ fontSize: '16px', fontFace: 'Arial', fontColor: 'red' }],
     },
@@ -37,7 +39,7 @@ export class SurveyCreationDesignComponent implements OnInit {
     },
     {
       componentType: 'text-editor',
-      componentText: this.sampleHtml,
+      componentText: this.sampleHtml2,
       componentSettings: [{ fontSize: '16px', fontFace: 'Arial', fontColor: 'red' }],
     },
   ];
@@ -87,14 +89,18 @@ export class SurveyCreationDesignComponent implements OnInit {
     this.model.editorData = this.sampleHtml;
   }
 
-  
+
   createNewBmxComponent() {
     window.scrollTo(300, 500);
     this.brandMatrixObjects.push({
       componentType: 'text-editor',
-      componentText: this.sampleHtml,
+      componentText: this.sampleHtml2,
       componentSettings: [{ fontSize: '16px', fontFace: 'Arial', fontColor: 'red' }],
     })
+  }
+
+  toggleInstructions() {
+    this.displayInstructions = !this.displayInstructions;
   }
 
   checkDragEvetn(e) {
