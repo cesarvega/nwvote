8caa584e55c35992b80403fc46afc9af2401a50f
cesarvega <cesarvega.col@gmail.com>
2021-08-04 13:50:56 -0400

    rating scale template dynamic columns in progress

--- src/app/bmx/bmx-creator/survey-creation-design/templates/rating-scale/rating-scale.component.ts
+++ src/app/bmx/bmx-creator/survey-creation-design/templates/rating-scale/rating-scale.component.ts
@@ -9,8 +9,6 @@ export class RatingScaleComponent implements OnInit {
 
   @Input() bmxItem;
   @Input() i;
-  @Input() isMenuActive11;
- 
   
   rankingScaleValue = 5;
   selectedIndex: any
@@ -19,17 +17,20 @@ export class RatingScaleComponent implements OnInit {
   selectedStarRatingIndex = ''
   selectedRating = 0;
 
+
+  // CONFIGURATION VARIABLES
+  testNamesInput:string
+  TestNameDataModel: any[];
+  ratingScale = 5;
+  TESTNAMES_LIST = [];
+ 
+
   constructor() { }
-
   ngOnInit(): void {
-    
   }
-
   setRating(starId, testNameId) {
-
     // prevent multiple selection
     if (this.selectedRating === 0) {
-
       this.bmxItem.componentText[testNameId].STARS.filter((star) => {
 
         if (star.id <= starId) {
@@ -44,12 +45,9 @@ export class RatingScaleComponent implements OnInit {
 
         return star;
       });
-
     }
   }
-
   selectStar(starId, testNameId): void {
-
     // prevent multiple selection
     if (this.selectedRating === 0) {
 
@@ -71,6 +69,56 @@ export class RatingScaleComponent implements OnInit {
 
     // this.selectedRating = value;
 
+  }
+
+  upLoadNamesAndRationales(list: string){
+    const textAreaInput = list.split("\n");
+
+    const columnsNames = textAreaInput[0].split("\t");
+
+    textAreaInput.splice(0,1);
+
+    for(var i = 0; i < textAreaInput.length; i++)
+    {
+      if(textAreaInput[i] != "")
+      {
+        this.TESTNAMES_LIST.push({'name': textAreaInput[i].split("\t")[0], 'rationale':textAreaInput[i].split("\t")[1]});
+      }
+    }
+
+    this.TestNameDataModel = [];
+    for (let index = 0; index <  this.TESTNAMES_LIST.length; index++) {
+      this.TestNameDataModel.push({
+        name: this.TESTNAMES_LIST[index].name, rationale: this.TESTNAMES_LIST[index].rationale,
+        STARS: this.createRatingStars(this.rankingScaleValue)
+      })
+    }
+
+     this.bmxItem.componentText =  this.TestNameDataModel;
+  }
+
+  deletPart(option: string): void {
+    for(var i = 0; i < this.TESTNAMES_LIST.length; i++)
+    {
+      if(this.TESTNAMES_LIST[i].name == option)
+      {
+        this.TESTNAMES_LIST.splice(i, 1);
+        break;
+      }
+    }    
+  }
+
+  // PRIVATE METHODS
+  createRatingStars(ratingScale) {
+    let startCounter: any = []
+    for (let index = 0; index < ratingScale; index++) {
+      startCounter.push({
+        id: index,
+        icon: 'grade',
+        class: 'rating-star'
+      });
+    }
+    return startCounter;
   }
 
 
