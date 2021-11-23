e151908baf677812ffdc16dac16111adbbc4e006
cesarvega <cesarvega.col@gmail.com>
2021-11-23 12:33:49 -0500

    merge code with kenneth

--- src/app/test/test/test.component.ts
+++ src/app/test/test/test.component.ts
@@ -1,10 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of, from, } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+interface Person {
+  name: string;
+  age: number;
+}
+
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.scss']
 })
+
+
+
 export class TestComponent implements OnInit {
   lengthOptions = [
     {
@@ -27,11 +38,39 @@ export class TestComponent implements OnInit {
 
   ngOnInit(): void {
 
+    const person: Person[] = [
+      {
+        name: 'John',
+        age: 30,
+      },
+      {
+        name: 'Cesar',
+        age: 30,
+      }
+    ];
+
+    const person$: Observable<Person> = from(person);
+    const personPromise: Promise<Person[]> = Promise.resolve(person);
+    // person$.subscribe(p => console.log(p.name));
+    const personFromPromise$: Observable<Person[]> = from(personPromise);
+
+    personFromPromise$.subscribe((p:Person[]) => {
+      // p.map(p => console.log(p.name));
+    });
+
+    personFromPromise$.pipe(
+      map((p, index) => p[0].name.toUpperCase()),
+    ).subscribe(c => {
+      console.log(c)
+    });
+
+    
+
 
   }
 
   myfunc() {
-  
+
   }
 
 }
