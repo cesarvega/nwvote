22fc2edf847f97eae9da0a5efe778a14d80c8028
cesarvega <cesarvega.col@gmail.com>
2022-06-02 18:42:59 -0400

    company filed updated to allow

--- src/app/bmx/bmx-creator/survey-creation-design/templates/tinder/tinder.component.ts
+++ src/app/bmx/bmx-creator/survey-creation-design/templates/tinder/tinder.component.ts
@@ -187,99 +187,17 @@ export class TinderComponent extends RatingScaleComponent implements OnInit {
     }
   }
 
-  moveRight() {
+  moveRight(testName:string) {
     if (this.testNameIndex < this.bmxItem.componentText.length - 1) {
       this.testNameIndex++
+      this.bmxItem.componentText[testName].comments = this.bmxItem.componentText[testName].comments
     }
   }
 
-  moveleft() {
+  moveleft(testName:string) {
     if (1 < this.testNameIndex) {
       this.testNameIndex--
+      this.bmxItem.componentText[testName].comments = this.bmxItem.componentText[testName].comments
     }
   }
-
-  ASSIGNED_CRITERIA = []
-  CRITERIA = [
-    { name: 'Fit to Company Description', rate: 0 },
-    { name: 'Fit to Product Statement', rate: 0 },
-    { name: 'Fit to Product Overview', rate: 0 },
-    { name: 'Fit to Global Positioning', rate: 0 },
-    { name: 'Fit to Concept/Positioning', rate: 0 },
-    { name: 'Fit to Brand Vision', rate: 0 },
-    { name: 'Fit to Vision Statement or Product Description', rate: 0 },
-    { name: 'Fit to Product Concept/Description', rate: 0 },
-    { name: 'Fit to Product Line Concept', rate: 0 },
-    { name: 'Fit to Global Concept', rate: 0 },
-    { name: 'Fit to Program Concept', rate: 0 },
-    { name: 'Fit to Therapeutic Area', rate: 0 },
-    { name: 'Fit to Service Positioning', rate: 0 },
-    { name: 'Fit to Product Description', rate: 0 },
-    { name: 'Fit to Venue Concept', rate: 0 },
-    { name: 'Fit to Program Description', rate: 0 },
-    { name: 'Fit to Program Vision', rate: 0 },
-    { name: 'Fit to Value Proposition', rate: 0 },
-    { name: 'Fit to Technology Concept', rate: 0 },
-    { name: 'Fit to Vision', rate: 0 },
-    { name: 'Product Positioning', rate: 0 },
-    { name: 'Fit to Product Concept and Positioning', rate: 0 },
-    { name: 'Fit to Concept Statement', rate: 0 },
-    { name: 'Fit to Division Concept', rate: 0 },
-    { name: 'Fit to Mechanism of Action', rate: 0 },
-    { name: 'Fit to Brand Concept', rate: 0 },
-    { name: 'Fit to Product Range Concept', rate: 0 },
-    { name: 'Fit to Concept', rate: 0 },
-    { name: 'Fit to Trial Concept', rate: 0 },
-    { name: 'Fit to Product Features and Benefits', rate: 0 },
-    { name: 'Fit to Brand', rate: 0 },
-    { name: 'Fit to Company Concept', rate: 0 },
-    { name: 'S`adapter au Concept de produit', rate: 0 },
-    { name: 'Fit to Compound Concept', rate: 0 },
-    { name: 'Fit to Service Concept', rate: 0 },
-    { name: 'Fit to Product Vision', rate: 0 },
-    { name: 'Fit to Contract Concept', rate: 0 },
-    { name: 'Fit to Product', rate: 0 },
-    { name: 'Fit to Brand Essence', rate: 0 },
-    { name: 'Fit to Entity Objectives', rate: 0 },
-    { name: 'Brand Family Rankings', rate: 0 },
-    { name: 'Fit to Trial Overview', rate: 0 },
-    { name: 'Fit to Business Unit Concept', rate: 0 },
-    { name: 'Fit to X4P-001 WHIM Syndrome Program', rate: 0 },
-    { name: 'Fit to Product Positioning', rate: 0 },
-    { name: 'Fit to LEO Pharma Mission and Vision', rate: 0 },
-    { name: 'Fit to Product Profile', rate: 0 },
-    { name: 'Fit to Positioning', rate: 0 },
-    { name: 'Fit to Company Mission', rate: 0 },
-    { name: 'Fit to Therapy', rate: 0 },
-    { name: 'Fit to Class Concept', rate: 0 },
-    { name: '• S`adapter au Concept de produit', rate: 0 },
-    { name: 'Fit to Product Concept/S`adapter au Concept de produit', rate: 0 },
-    { name: 'Fit to Portfolio Concept', rate: 0 },
-    { name: 'Fit to Mission and Vision Statements', rate: 0 },
-    { name: 'Overall Feasibility', rate: 0 },
-    { name: 'Fit to Company Description/Mission', rate: 0 },
-    { name: 'Fit to Compound Character and Image', rate: 0 },
-    { name: 'Fit to Strategy', rate: 0 },
-    { name: 'Personal Preference', rate: 0 },
-    { name: 'OPSIRIA Likeness', rate: 0 },
-    { name: 'Appropriately describes the Flutiform breath triggered inhaler', rate: 0 },
-    { name: 'Overall strategic fit and likeability', rate: 0 },
-    { name: 'Uniqueness', rate: 0 },
-    { name: 'Fit to Category Concept', rate: 0 },
-    { name: 'Overall Preference', rate: 0 },
-    { name: 'Connection to Hemlibra', rate: 0 },
-    { name: 'Fit to Company Vision Statement', rate: 0 },
-    { name: 'Fit to Website Concept', rate: 0 },
-    { name: 'Dislike', rate: 0 },
-    { name: 'Like', rate: 0 },
-    { name: 'Negative/Offensive Communication', rate: 0 },
-    { name: 'Fit to Phase 2/3 HTE Trial', rate: 0 },
-    { name: 'Fit to Phase 2 VS Trial', rate: 0 },
-    { name: 'Exaggerative/Inappropriate Claim', rate: 0 },
-    { name: 'Fit to Product Concept', rate: 0 },
-    { name: 'Attribute Evaluations', rate: 0 },
-    { name: 'Memorability', rate: 0 },
-    { name: 'Overall Likeability', rate: 0 },
-    { name: 'How the test name works alongside the name CUVITRU?', rate: 0 },
-  ]
 }
