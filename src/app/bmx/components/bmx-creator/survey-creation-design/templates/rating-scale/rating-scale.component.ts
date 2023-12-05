94c732e29136864b5d42fc15d6e79a3e63bc2db0
unknown <braian.morales@correunivalle.edu.co>
2023-12-05 17:02:19 -0500

    Show short menu on init

--- src/app/bmx/components/bmx-creator/survey-creation-design/templates/rating-scale/rating-scale.component.ts
+++ src/app/bmx/components/bmx-creator/survey-creation-design/templates/rating-scale/rating-scale.component.ts
@@ -72,7 +72,7 @@ export class RatingScaleComponent implements OnInit {
   columnFontSize = 15;
   randomizeTestNames = false
   displaySound = false
-  showMatrixMenu: boolean = true;
+  showMatrixMenu: boolean = false;
   iconMenuShow: string = "add_circle_outline";
   textToolTip: string = "open menu";
   scroll: any; s
