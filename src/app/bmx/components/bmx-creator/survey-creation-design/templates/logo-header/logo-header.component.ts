05095473e6d89452560a3e70953dfd27e003969e
Braian <braian.morales@orusxpert.com>
2024-03-01 17:16:40 -0500

    Fix logo upload

--- src/app/bmx/components/bmx-creator/survey-creation-design/templates/logo-header/logo-header.component.ts
+++ src/app/bmx/components/bmx-creator/survey-creation-design/templates/logo-header/logo-header.component.ts
@@ -23,6 +23,7 @@ export class LogoHeaderComponent implements OnInit {
   @Input() bmxClientPageDesignMode;
   @Input() bmxClientPageOverview;
   @Input() isBrandMatrixSurvey;
+  @Input() creationMode = false
   displayInstructions: boolean;
   openSettings = false;
   displayLogoWidthRange = false;
@@ -39,18 +40,17 @@ export class LogoHeaderComponent implements OnInit {
   IMAGES_UPLOADED = [];
   projectId: any;
   biUsername: any;
-  imageLogoSrc 
-  showLogoIcon = false;  
-
-  constructor(private _BmxService: BmxService,private el: ElementRef) {}
+  imageLogoSrc
+  showLogoIcon = false;
+  constructor(private _BmxService: BmxService, private el: ElementRef) { }
 
   ngOnInit(): void {
-    this.bmxItem.componentText = (localStorage.getItem('projectId'))?localStorage.getItem('projectId'):this.bmxItem.componentText;
+    this.bmxItem.componentText = (localStorage.getItem('projectId')) ? localStorage.getItem('projectId') : this.bmxItem.componentText;
     this.imageLogoSrc = this.bmxItem.componentSettings[0].companyLogoURL
   }
 
   onFileSelected(event) {
-    if (event.target.files && event.target.files[0]) {
+    if (event.target.files && event.target.files[0] && !this.creationMode) {
       let filesAmount = event.target.files.length;
       let fileName = event.target.files[0].name;
       let fileType = event.target.files[0].type;
@@ -86,7 +86,7 @@ export class LogoHeaderComponent implements OnInit {
     }
   }
 
-  resizeLogo(event: any){
+  resizeLogo(event: any) {
     this.resizeWidthLogo.emit(event)
     this._BmxService.setLogoTemporaryWidth(event)
   }
@@ -104,8 +104,8 @@ export class LogoHeaderComponent implements OnInit {
       });
   }
 
-  deleteIcon(){
-    this.bmxItem.componentSettings[0].companyLogoURL = (this.bmxItem.componentSettings[0].companyLogoURL == '')?this.imageLogoSrc:'';
+  deleteIcon() {
+    this.bmxItem.componentSettings[0].companyLogoURL = (this.bmxItem.componentSettings[0].companyLogoURL == '') ? this.imageLogoSrc : '';
   }
 
   addLogo
