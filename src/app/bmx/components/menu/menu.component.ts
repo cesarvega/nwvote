8c5e4b415416612aae4dbd01db4d128c72145369
cesarvega <cesarvega.col@gmail.com>
2024-03-01 14:19:59 -0500

    fix calendar background

--- src/app/bmx/components/menu/menu.component.ts
+++ src/app/bmx/components/menu/menu.component.ts
@@ -26,7 +26,7 @@ export class MenuComponent implements OnInit {
   userDepartment: string;
   userOffice: any;
   id: string;
-  versionNumber = 'v1.0.13';
+  versionNumber = 'v1.0.14';
 
   // constructor(private router: Router, private _BmxService: BmxService, private activatedRoute: ActivatedRoute,) {
 
