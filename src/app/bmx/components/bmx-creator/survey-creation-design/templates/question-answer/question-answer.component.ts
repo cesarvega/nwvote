db1d3f8811eed40269aefe35f88a1bd51ceb7342
Braian <braian.morales@orusxpert.com>
2024-03-04 14:57:54 -0500

    remove first line

--- src/app/bmx/components/bmx-creator/survey-creation-design/templates/question-answer/question-answer.component.ts
+++ src/app/bmx/components/bmx-creator/survey-creation-design/templates/question-answer/question-answer.component.ts
@@ -35,8 +35,36 @@ export class QuestionAnswerComponent extends RatingScaleComponent implements OnI
         this.columnsNames.push(value)
       }
     });
+    let result = '';
+
+    // Obtener las claves de la primera fila (los nombres de las propiedades)
+    let firstObject = this.bmxItem.componentText[0];
+    let columnNames = [];
+    for (let key in firstObject) {
+      if (key !== 'STARS' && key !== 'RATE' && key !== 'nameCandidates' && key !== 'rationale') {
+        columnNames.push(key);
+      }
+    }
+
+    // Construir la primera fila con los nombres de las columnas
+    result += columnNames.join('\t') + '\n';
+
+    // Agregar cada objeto como una fila en el resultado
+    for (let obj of this.bmxItem.componentText) {
+      let values = [];
+      for (let key in obj) {
+        if (key !== 'STARS' && key !== 'RATE') {
+          values.push(obj[key]);
+        }
+      }
+      result += values.join('\t') + '\n';
+    }
+
+    this.testNamesInput = result
     this.randomizeTestNames = this.bmxItem.componentSettings[0].randomizeTestNames
     this.rowsCount = this.bmxItem.componentText.length - 1;
+
+    
   }
 
   upLoadNamesAndRationales(list: string) {
