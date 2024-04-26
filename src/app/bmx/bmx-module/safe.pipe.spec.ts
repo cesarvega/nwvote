bae7a879c19f4687fb07e63b92127feaae78e900
cesarvega <cesarvega.col@gmail.com>
2024-04-26 19:09:32 -0400

    remove unused files

--- src/app/bmx/bmx-module/safe.pipe.spec.ts
+++
@@ -1,9 +0,0 @@
-import { SafePipe } from './safe.pipe';
-import { DomSanitizer, SafeHtml, SafeStyle, SafeScript, SafeUrl, SafeResourceUrl } from '@angular/platform-browser';
-
-// describe('SafePipe', () => {
-//   it('create an instance', () => {
-//     const pipe = new SafePipe(publisanitizer: DomSanitizer);
-//     expect(pipe).toBeTruthy();
-//   });
-// });
