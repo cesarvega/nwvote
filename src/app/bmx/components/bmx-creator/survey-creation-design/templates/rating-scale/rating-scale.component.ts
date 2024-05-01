deb9cdb3014c31c37edf474756e8204db8ac0d62
Braian <braian.morales@orusxpert.com>
2024-05-01 15:55:06 -0500

    Fixed bugs

--- src/app/bmx/components/bmx-creator/survey-creation-design/templates/rating-scale/rating-scale.component.ts
+++ src/app/bmx/components/bmx-creator/survey-creation-design/templates/rating-scale/rating-scale.component.ts
@@ -443,6 +443,7 @@ export class RatingScaleComponent implements OnInit {
   // CRITERIA STARS
 
   setCriteriaRating(starId, criteriaId, testNameId) {
+    console.log('aaaaaaa')
     let intCounter = 0
     this.bmxItem.componentText[testNameId].CRITERIA.forEach((criteria) => {
       intCounter = intCounter + criteria.RATE
