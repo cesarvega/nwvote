5f1115ce05e7df6757a5417456176aa9b764fa0e
Saray Meneses <gonzalezmeneses1234@gmail.com>
2024-05-03 08:44:32 -0500

    styles online

--- src/app/bmx/components/menu/menu.component.ts
+++ src/app/bmx/components/menu/menu.component.ts
@@ -31,7 +31,7 @@ export class MenuComponent implements OnInit {
   userDepartment: string;
   userOffice: any;
   id: string;
-  versionNumber = 'v1.0.22';
+  versionNumber = 'v1.0.23';
 
   // constructor(private router: Router, private _BmxService: BmxService, private activatedRoute: ActivatedRoute,) {
 
