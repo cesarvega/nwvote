e509c8882d3473d33434cd88432246f61b9e0e0d
bi\cvega <cvega@brandinstitute.com>
2024-10-31 11:54:18 -0400

    change version

--- src/app/bmx/components/menu/menu.component.ts
+++ src/app/bmx/components/menu/menu.component.ts
@@ -32,7 +32,7 @@ export class MenuComponent implements OnInit {
   userDepartment: string;
   userOffice: any;
   id: string;
-  versionNumber = 'v1.0.13';
+  versionNumber = 'v1.0.12';
   showErrorMessage = false;
 
   // constructor(private router: Router, private _BmxService: BmxService, private activatedRoute: ActivatedRoute,) {
